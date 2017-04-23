7868f40849b91f064d6f9bbd3cc03a95c1974790
bigYelloDuck <givemeyourfox@gmail.com>
2017-04-23 20:50:29 +0800

    add buffer for connection

--- star/tcpconn.cpp
+++ star/tcpconn.cpp
@@ -1,11 +1,34 @@
 #include "tcpconn.h"
 #include "logging.h"
 #include "acceptor.h"
+#include "channel.h"
 
 #include <unistd.h>
 #include <string.h>
+#include <sys/uio.h>
 
 namespace star {
+
+int ReadBuffer::readFd(int fd) {
+    char extrabuf[65536];
+    struct iovec vec[2];
+    const size_t writable = avail();
+    vec[0].iov_base = const_cast<char*>(buf_.data()+endOfData_);
+    vec[0].iov_len = writable;
+    vec[1].iov_base = extrabuf;
+    vec[1].iov_len = sizeof(extrabuf);
+    ssize_t n = readv(fd, vec, 2);
+    if (n<0) {
+        error("readBuffer readv error %d %s", errno, strerror(errno));
+    } else if (static_cast<size_t>(n) <= writable) {
+        addIndex(n);
+    } else {
+        setIndex(bufSize());
+        append(extrabuf, n - writable);
+    }
+
+    return n;
+}
 
 TcpConnection::TcpConnection(EventLoop* loop,
                              int sockfd,
@@ -15,7 +38,7 @@ TcpConnection::TcpConnection(EventLoop* loop,
       channel_(new Channel(loop_, sockfd)),
       connId_(connId),
       state_(kConnecting) {
-    
+    socket_->setNoDelay(true);    
 }
 
 TcpConnection::~TcpConnection() {
@@ -37,18 +60,40 @@ void TcpConnection::connectionEstablished() {
 
 void TcpConnection::handleRead() {
     trace("connection %d handleRead", connId_);
-    char buf[65536];
-    ssize_t n = ::read(channel_->fd(), buf, sizeof(buf));
-    
+
+   ssize_t n = readBuffer_.readFd(channel_->fd());
+
     if (n>0) {
         if (messageCallback_) { 
             trace("call messageCallback");
-            messageCallback_(shared_from_this(), buf, n);
+            messageCallback_(shared_from_this(), &readBuffer_);
         }
     } else if (n==0) {
         handleClose();
     } else {
         handleError();
+    }
+}
+
+void TcpConnection::handleWrite() {
+    trace("connection %d handleWrite", connId_);
+    if (channel_->isWriting()) {
+        ssize_t n = ::write(channel_->fd(),
+                            writeBuffer_.dataForSend(),
+                            writeBuffer_.readyBytes());
+        if (n>0) {
+            writeBuffer_.retrieve(n);
+            if (writeBuffer_.empty()) {
+                channel_->enableWrite(false);
+                channel_->updateToPoller();
+            } else {
+                trace("connection %d is going to send more data", connId_);
+            }
+        } else {
+            error("connection %d handleWrite error %d", connId_, errno);
+        }
+    } else {
+        trace("connection %d is down, can't write", connId_);
     }
 }
 
@@ -65,6 +110,28 @@ void TcpConnection::handleError() {
     info("TcpConnectionPtr::handleError %d %s", errno, strerror(errno));
 }
 
+void TcpConnection::send(const std::string& message) {
+    ssize_t nwrote = 0;
+    if (!channel_->isWriting() && writeBuffer_.empty()) {
+        nwrote = ::write(channel_->fd(), message.data(), message.size());
+        if (nwrote >= 0) {
+            if (static_cast<size_t>(nwrote) < message.size())
+                trace("connect %d is going to send more data", connId_);
+        } else {
+            nwrote = 0;
+            if (errno != EWOULDBLOCK)
+                error("connection %d send error %d", connId_, errno);
+        }
+    }
 
+    if (static_cast<size_t>(nwrote) < message.size()) {
+        writeBuffer_.append(message.data(), message.size());
+        if (!channel_->isWriting()) {
+            channel_->setWriteCallBack([this]{handleWrite();});
+            channel_->enableWrite(true);
+            channel_->updateToPoller();
+        }
+    }
+}
 
 }  // end of namespace star
--- star/tcpconn.h
+++ star/tcpconn.h
@@ -1,21 +1,87 @@
 #ifndef STARSERVER_TCPCONN_H
 #define STARSERVER_TCPCONN_H
 
+
 #include <memory>
 #include <functional>
+#include <string>
+#include <algorithm>
 
 #include "util.h"
 #include "eventloop.h"
-#include "channel.h"
 
 namespace star {
 
+class ReadBuffer;
 class Socket;
 class TcpConnection;
 using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
 using ConnectionCallback = std::function<void(const TcpConnectionPtr&)>;
-using MessageCallback = std::function<void(const TcpConnectionPtr&, const char*, size_t)>;
+using MessageCallback = std::function<void(const TcpConnectionPtr&, ReadBuffer*)>;
 using CloseCallback = std::function<void(const TcpConnectionPtr&)>;
+
+class Buffer : public Noncopyable {
+  public:
+    Buffer():endOfData_(0) { buf_.resize(65536); }
+    void append(const char* data, size_t len) {
+        if (avail() > len) {
+           copy(data, data+len, buf_.begin()+endOfData_); 
+           endOfData_ += len;
+        } else {      // FIXME:find a better way to deal with memory increase problem
+            std::string exbuf;
+            exbuf.resize(buf_.size() * 2);
+            buf_.swap(exbuf);
+            copy(exbuf.begin(), exbuf.begin()+endOfData_, buf_.begin());
+        }
+    }
+    
+    size_t len() const noexcept { return endOfData_; }
+    size_t bufSize() const noexcept { return buf_.size(); }
+    size_t avail() const noexcept { return buf_.size() - endOfData_; }
+  protected:
+    void addIndex(size_t n) { endOfData_ += n; }
+    void setIndex(size_t n) { endOfData_ = n; }
+
+    std::string buf_;
+    size_t endOfData_;
+};
+
+class ReadBuffer : public Buffer {
+  public:
+    ReadBuffer()
+        : Buffer() {
+    }
+    
+    int readFd(int fd);
+    const std::string retrieveAsString() noexcept {
+        std::string str(buf_.begin(), buf_.begin()+endOfData_);
+        endOfData_ = 0;
+        return str;
+    }
+};
+
+class WriteBuffer : public Buffer {
+  public:
+    WriteBuffer()
+        : Buffer(),
+          indexForSend_(0) {
+    }
+
+    void retrieve(size_t n) { 
+        indexForSend_ += n; 
+        if (indexForSend_ == endOfData_) {
+            indexForSend_ = 0;
+            setIndex(0);
+        }
+    } 
+    const char* dataForSend() const noexcept { return buf_.data()+indexForSend_; }
+    
+    size_t readyBytes() const noexcept { return endOfData_ - indexForSend_; }
+    bool empty() const noexcept { return endOfData_ == indexForSend_; }
+
+  private:
+    size_t indexForSend_;
+};
 
 class TcpConnection : public Noncopyable, 
                       public std::enable_shared_from_this<TcpConnection> {
@@ -52,11 +118,13 @@ class TcpConnection : public Noncopyable,
     int connId() const noexcept { return connId_; }
     bool connected() const noexcept { return state_ == KConnected; }
 
+    void send(const std::string& message);
   private:
     enum State {kConnecting, KConnected, kDisconnected};
     void setState(State s) { state_ = s;}
 
     void handleRead();
+    void handleWrite();
     void handleClose();
     void handleError();
 
@@ -65,12 +133,13 @@ class TcpConnection : public Noncopyable,
     std::unique_ptr<Channel> channel_;
     int connId_;
     State state_;
+    ReadBuffer readBuffer_;
+    WriteBuffer writeBuffer_;
 
     ConnectionCallback connectionCallback_;
     MessageCallback messageCallback_;
     CloseCallback closeCallback_;
 };
-
 
 
 }  // end of namespace star
