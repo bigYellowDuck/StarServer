8a006f0cade4201ce194c0840df5318103dbf3be
bigYelloDuck <givemeyourfox@gmail.com>
2017-04-25 00:20:41 +0800

    setThreads

--- star/server.cpp
+++ star/server.cpp
@@ -25,12 +25,19 @@ void Server::removeConnection(const TcpConnectionPtr& conn) {
     connections_.erase(conn->connId());
 }
 
+void Server::setThreadNum(int numThreads) {
+    multiLoop_ = std::move(std::unique_ptr<MultiEventLoop>(new MultiEventLoop(loop_.get())));
+    multiLoop_->setThreadNum(numThreads);
+    multiLoop_->start();
+}
+
 void Server::newConnection(int sockfd, struct sockaddr_in* addr) {
     char buf[16];
     snprintf(buf, sizeof(buf), "#%d", nextConnId_);
     trace("server accept a new connection %s", buf);
 
-    TcpConnectionPtr conn = std::make_shared<TcpConnection>(loop_.get(), sockfd, nextConnId_);
+    EventLoop* ioLoop = multiLoop_->getNextLoop();
+    TcpConnectionPtr conn = std::make_shared<TcpConnection>(ioLoop, sockfd, nextConnId_);
     connections_[nextConnId_] = conn;
     conn->setConnectionCallback(connectionCallback_);
     conn->setMessageCallback(messageCallback_);
@@ -38,8 +45,10 @@ void Server::newConnection(int sockfd, struct sockaddr_in* addr) {
             [this](const TcpConnectionPtr& conn) {
                 this->removeConnection(conn);
             });
-    conn->connectionEstablished();
-    
+    loop_->runInLoopThread([conn] {
+            conn->connectionEstablished();
+        });
+
     ++nextConnId_;
     (void)addr;
 }
--- star/server.h
+++ star/server.h
@@ -36,14 +36,17 @@ class Server : public Noncopyable {
     }
 
     void removeConnection(const TcpConnectionPtr& conn);
+
+    void setThreadNum(int numThreads);
   private:
     void newConnection(int sockfd, struct sockaddr_in* addr);
     
     std::unique_ptr<EventLoop> loop_;
     std::unique_ptr<Acceptor> acceptor_;
+    std::unique_ptr<MultiEventLoop> multiLoop_;
     int nextConnId_;
     ConnectionMap connections_;
-
+    
     ConnectionCallback connectionCallback_;
     MessageCallback messageCallback_;
 };
